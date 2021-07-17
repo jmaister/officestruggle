793e8f69b62638fdfb5ea60256bfb3e7cd3771bd
Jordi Burgos <jordiburgos@gmail.com>
2021-07-18 01:13:37 +0200

    Create assets module

---
+++ assets/assets.go
@@ -0,0 +1,78 @@
+package assets
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/golang/freetype/truetype"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+)
+
+//go:embed fonts/*
+var Data embed.FS
+
+var (
+	mplusFontCached map[float64]font.Face
+)
+
+var fontCached = map[float64]font.Face{}
+
+func MplusFont(size float64) font.Face {
+	if mplusFontCached == nil {
+		mplusFontCached = map[float64]font.Face{}
+	}
+	fnt, ok := mplusFontCached[size]
+	if !ok {
+		// tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+		tt, err := opentype.Parse(fonts.PressStart2P_ttf)
+		if err != nil {
+			panic(err)
+		}
+
+		fnt, err = opentype.NewFace(tt, &opentype.FaceOptions{
+			Size:    size,
+			DPI:     72,
+			Hinting: font.HintingFull,
+		})
+		if err != nil {
+			panic(err)
+		}
+		mplusFontCached[size] = fnt
+	}
+	return fnt
+}
+
+func LoadFontCached(size float64) font.Face {
+	fnt, ok := fontCached[size]
+	if !ok {
+		fnt = loadFont(size)
+		fontCached[size] = fnt
+	}
+	return fnt
+}
+
+func loadFont(size float64) font.Face {
+	//fontFile := "fonts/NotCourierSans.ttf"
+	// fontFile := "fonts/DejaVuSansMono-Bold.ttf"
+	//fontFile := "fonts/SLC_.ttf"
+	//fontBytes, err := ioutil.ReadFile(fontFile)
+
+	fontFile := "fonts/FreeMono.ttf"
+	fontBytes, err := Data.ReadFile(fontFile)
+
+	if err != nil {
+		panic(err)
+	}
+	tt, err := truetype.Parse(fontBytes)
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing font bytes: %v", err))
+	}
+
+	return truetype.NewFace(tt, &truetype.Options{
+		Size:    size,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module jordiburgos.com/officestruggle
 go 1.16
 
 require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.1.3
 	github.com/norendren/go-fov v1.0.1
