9f26600d17b71334cecfdf4934893601f50b4864
Jordi Burgos <jordiburgos@gmail.com>
2021-07-21 20:59:51 +0200

    Create list UI component.

--- systems/inventoryscreen.go
+++ systems/inventoryscreen.go
@@ -9,6 +9,7 @@ import (
 	"jordiburgos.com/officestruggle/assets"
 	"jordiburgos.com/officestruggle/ecs"
 	"jordiburgos.com/officestruggle/gamestate"
+	"jordiburgos.com/officestruggle/grid"
 	"jordiburgos.com/officestruggle/state"
 )
 
@@ -17,8 +18,28 @@ func RenderInventoryScreen(engine *ecs.Engine, gameState *gamestate.GameState, s
 	text.Draw(screen, "Inventory", fnt40, 30, 35, color.White)
 	text.Draw(screen, "C - Consume, D - Drop, E - Equip, U - Unequip", fnt20, 300, 40, color.White)
 
-	drawFullInventory(screen, gameState)
-	drawEquipment(screen, gameState)
+	//drawFullInventory(screen, gameState)
+	//drawEquipment(screen, gameState)
+
+	// Inventory
+	inventory, _ := gameState.Player.GetComponent(state.Inventory).(state.InventoryComponent)
+	inventoryTitle := "Inventory " + strconv.Itoa(len(inventory.Items)) + "/" + strconv.Itoa(inventory.MaxItems)
+	drawList(screen, gameState, &ListState{
+		Selected:  0,
+		IsFocused: true,
+	}, inventory.Items, gameState.Grid.Inventory, inventoryTitle)
+
+	// Equipment
+	equipment, _ := gameState.Player.GetComponent(state.Equipment).(state.EquipmentComponent)
+	equipmentTitle := "Equipment"
+	items := ecs.EntityList{}
+	for _, item := range equipment.Items {
+		items = append(items, item)
+	}
+	drawList(screen, gameState, &ListState{
+		Selected:  0,
+		IsFocused: false,
+	}, items, gameState.Grid.Equipment, equipmentTitle)
 }
 
 func drawFullInventory(screen *ebiten.Image, gs *gamestate.GameState) {
@@ -86,6 +107,38 @@ func drawEquipment(screen *ebiten.Image, gs *gamestate.GameState) {
 		text.Draw(screen, "- No items in the inventory -", font, (position.X)*fontSize, (y+1)*fontSize, color.White)
 	}
 
+}
+
+type ListState struct {
+	Selected  int
+	IsFocused bool
+}
+
+func drawList(screen *ebiten.Image, gs *gamestate.GameState, listState *ListState, items ecs.EntityList, position grid.Rect, title string) {
+	fontSize := 14
+	font := assets.MplusFont(float64(fontSize))
+
+	y := position.Y
+	text.Draw(screen, title, font, (position.X)*fontSize, y*fontSize, color.White)
+
+	if len(items) > 0 {
+		for i, entity := range items {
+			selected := (i == listState.Selected)
+
+			str := strconv.Itoa(i) + "-" + state.GetLongDescription(entity)
+			px := (position.X) * fontSize
+			py := (y + 1) * fontSize
+			if selected && listState.IsFocused {
+				DrawTextRect(screen, str, px, py, font, color.White)
+				text.Draw(screen, str, font, px, py, color.Black)
+			} else {
+				text.Draw(screen, str, font, px, py, color.White)
+			}
+			y++
+		}
+	} else {
+		text.Draw(screen, "- No items -", font, (position.X)*fontSize, (y+1)*fontSize, color.White)
+	}
 }
 
 func InventoryKeyUp(gs *gamestate.GameState) {
