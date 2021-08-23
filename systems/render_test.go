27917fca6b356243fc8ebdf245a3e24329f9fbb0
Jordi Burgos <jordiburgos@gmail.com>
2021-08-23 19:33:49 +0200

    Render improvements and test.

--- systems/render_test.go
+++ systems/render_test.go
@@ -1,9 +1,13 @@
 package systems_test
 
 import (
+	"image"
+	"image/png"
+	"os"
 	"testing"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"jordiburgos.com/officestruggle/constants"
 	"jordiburgos.com/officestruggle/ecs"
 	"jordiburgos.com/officestruggle/game"
 	"jordiburgos.com/officestruggle/gamestate"
@@ -11,24 +15,50 @@ import (
 )
 
 func TestRender(t *testing.T) {
-	engine := ecs.NewEngine()
-	gs := game.NewGameState(engine)
+	g := game.NewGame()
+	engine := g.Engine
+	gs := g.GameState
 
 	gs.ScreenState = gamestate.GameScreen
 	systems.ComputeFov(engine, gs)
+	gs.Log(constants.Good, "Testing the game.")
 
 	screen := ebiten.NewImage(gs.ScreenWidth, gs.ScreenHeight)
 
 	systems.Render(engine, gs, screen)
+
+	imgToSave := image.NewRGBA(image.Rectangle{
+		Min: image.Point{
+			X: 0,
+			Y: 0,
+		},
+		Max: image.Point{
+			X: gs.ScreenWidth,
+			Y: gs.ScreenHeight,
+		},
+	})
+	for x := 0; x < gs.ScreenWidth; x++ {
+		for y := 0; y < gs.ScreenHeight; y++ {
+			cl := screen.At(x, y)
+			imgToSave.Set(x, y, cl)
+		}
+	}
+
+	f, err := os.Create("img.png")
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(f, imgToSave)
+
 }
 
 func BenchmarkRender(b *testing.B) {
 	engine := ecs.NewEngine()
 	gs := game.NewGameState(engine)
-	gs.ScreenState = gamestate.GameScreen
 
 	gs.ScreenState = gamestate.GameScreen
 	systems.ComputeFov(engine, gs)
+	gs.Log(constants.Good, "Testing the game.")
 
 	screen := ebiten.NewImage(gs.ScreenWidth, gs.ScreenHeight)
 
